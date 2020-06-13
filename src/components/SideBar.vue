75400d494af03aaf8c1c7c7e2f1c0a002fa8c994
Vanessa Joy Burgos <vienna@unnotechmanila.com>
2020-06-13 11:15:23 +0800

    Removed Web Apps Page

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -122,9 +122,9 @@
         drawer: null,
         items: [
           { icon: 'apps', text: this.$t('nav.apps'), path: '/apps' },
-          { heading: this.$t('nav.website_view') },
-          { icon: 'view_quilt', text: this.$t('nav.website_apps'), path: '/webapps'},
           { icon: 'web', text: this.$t('nav.websites'), path: '/websites'},
+          { heading: this.$t('nav.website_view') },
+          // { icon: 'view_quilt', text: this.$t('nav.website_apps'), path: '/webapps'},
           { icon: 'new_releases', text: this.$t('nav.types'), path: '/types?website=1'},
           { icon: 'category', text: this.$t('nav.category'), path: '/category?website=1'},
           { icon: 'label', text: this.$t('nav.labels'), path: '/label?website=1'},
